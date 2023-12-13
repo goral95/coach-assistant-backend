83596a35075ad68d0adfab28aac33a793e44cc52
Lukasz Gorczyk <lukasz.gorczyk.programista@gmail.com>
2023-12-13 20:40:59 +0100

    Start of project

---
+++ src/main/java/com/example/coachassistantbackend/CoachAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.coachassistantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachAssistantBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coachassistantbackend/CoachAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coachassistantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachAssistantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
